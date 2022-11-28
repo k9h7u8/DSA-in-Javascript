9b74178db3d78c761c40d95e2e1f514b733970dd
Khushi Aggarwal <khushiaggarwal1302@gmail.com>
2022-11-28 23:26:13 +0530

    maths ques

---
+++ maths/sum-natural-no.js
@@ -0,0 +1,16 @@
+// Solution with time complexity = O(n)
+function sumNatural(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+console.log(sumNatural(3));
+
+
+// Solution with time complexity = O(1)
+function sumOfNatural(n) {
+    return (n * (n + 1)) / 2;
+}
+console.log(sumOfNatural(6));
